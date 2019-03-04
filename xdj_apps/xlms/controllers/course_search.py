2357b25c987c8969e7bcf6e1db307c58c90fa54a
nttlong <zugeliang2000@gamil.com>
2019-03-04 10:51:23 +0700

    save

--- xdj_apps/xlms/controllers/course_search.py
+++ xdj_apps/xlms/controllers/course_search.py
@@ -15,25 +15,14 @@ class CourseSearchController(EdxController):
 
     def on_get(self,model):
         return self.render(model)
+
     def doSearch(self,model):
         from contentstore.courseware_index import CourseAboutSearchIndexer
         from qres.filter import Fields
+
         if not hasattr(model.post_data,"value") or model.post_data.value == "":
             ret = self.get_es().search_as_objects(doc_type = CourseAboutSearchIndexer.DISCOVERY_DOCUMENT_TYPE,page_size=9)
-            for x in ret.items:
-                ret.items = [dict(
-                    start= x._source.start,
-                    display_name = x._source.content.display_name,
-                    image_url = x._source.image_url,
-                    org = x._source.org,
-                    number = x._source.number,
-                    id = x._source.id ,
-                    organization=x._source.organization,
-                    author=x._source.author,
-                    subject=x._source.subject
 
-                )  for x in ret.items]
-            return ret
         else:
             ret = self.get_es().search_as_objects(
                 doc_type="course_info",
@@ -42,19 +31,24 @@ class CourseSearchController(EdxController):
                         (Fields().subject.name << model.post_data.value)|
                         (Fields().organization.name << model.post_data.value) |
                         (Fields().author.username << model.post_data.value))
-            ret.items = [dict(
-                start=x._source.start,
-                display_name=x._source.content.display_name,
-                image_url=x._source.image_url,
-                org=x._source.org,
-                number=x._source.number,
-                id=x._source.id,
-                organization = x._source.organization,
-                author = x._source.author,
-                subject = x._source.subject
 
-            ) for x in ret.items]
-            return ret
+        ret_data = dict(total=ret.total, items=[], page_index=ret.page_index, page_size=ret.page_size)
+        for x in ret.items:
+
+            if hasattr(x._source,"organization"):
+                ret_data["items"].append(dict(
+                    start=x._source.start,
+                    display_name=x._source.content.display_name,
+                    image_url=x._source.image_url,
+                    org=x._source.org,
+                    number=x._source.number,
+                    id=x._source.id,
+                    organization=x._source.organization,
+                    author=x._source.author,
+                    subject=x._source.subject
+                ))
+        return ret_data
+
 
 
 
